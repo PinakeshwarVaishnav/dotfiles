c9b8f7e6cb8943e3ba91e39bccedbd95ddf7e500
Pinakeshwar Vaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2026-03-03 14:24:36 +0530

    build: fix hardcoded paths in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,14 @@
-set -gx PATH $HOME/.nvm/versions/node/v22.11.0/bin:$PATH
-
 # pnpm
-set -gx PNPM_HOME "/home/pinakeshwar/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+
+# fnm
+set -gx FNM_PATH "$HOME/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+  set PATH "$FNM_PATH" $PATH
+  fnm env | source
+end
