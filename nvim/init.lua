b04cb654b7b16ca7897efd4c3c94df7a119546e8
Pinakeshwar Vaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2026-06-29 14:15:59 +0530

    refactor: remove web development tools and plugins
    Removes web-related language servers (CSS, HTML, Tailwind, ESLint,
    TypeScript, Emmet, GraphQL), formatters (Prettier), linters
    (JSON/Markdown), and Treesitter syntax parsers.

--- nvim/init.lua
+++ nvim/init.lua
@@ -594,6 +594,7 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+
       local servers = {
         -- install the following commented names manually using mason:
         -- blackd-client,
@@ -606,26 +607,13 @@ require('lazy').setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
         ruff = {},
-        cssls = {},
-        emmet_language_server = {},
-        eslint = {},
-        graphql = {},
-        html = {},
-        jsonls = {},
         pyright = {},
         sqlls = {},
-        tailwindcss = {},
-        ts_ls = {},
         yamlls = {},
 
         lua_ls = {
+
           -- cmd = {...},
           -- filetypes = { ...},
           -- capabilities = {},
@@ -650,17 +638,13 @@ require('lazy').setup({
       require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         -- Formatters
         'stylua',
-        'prettierd',
         'yamlfmt',
 
         -- Linters
-        'jsonlint',
-        'markdownlint',
         'yamllint',
         'actionlint',
         'sqlfluff',
@@ -727,8 +711,6 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -917,7 +899,6 @@ require('lazy').setup({
         'bash',
         'c',
         'diff',
-        'html',
         'lua',
         'luadoc',
         'markdown',
@@ -925,9 +906,6 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
-        'javascript',
-        'typescript',
-        'tsx',
         'python',
       },
       -- Autoinstall languages that are not installed
